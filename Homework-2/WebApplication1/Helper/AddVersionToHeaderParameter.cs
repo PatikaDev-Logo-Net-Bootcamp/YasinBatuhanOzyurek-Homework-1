215019eb753087d1ab61bf2b9b43949fceab31cc
Yasin Batuhan Özyürek <yasinbatuhanozyurek@gmail.com>
2022-03-15 00:33:04 +0300

    add homework 2

---
+++ Homework-2/WebApplication1/Helper/AddVersionToHeaderParameter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace WebApplication1.Helper
+{
+    public class AddVersionToHeaderParameter : IOperationFilter
+    {
+        // class for putting header option in swagger
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "app-version",
+                In = ParameterLocation.Header,
+                Description = "Application version",
+                Schema = new OpenApiSchema
+                {
+                    Type = "string",
+                    Default = new OpenApiString("2.0.0")
+                }
+            });
+            
+        }
+    }
+}
---
+++ Homework-2/WebApplication1/Middlewares/AppVersionControllerMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Middlewares
+{
+
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class AppVersionControllerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public AppVersionControllerMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                // Optional: Get version from assembly using reflection
+                //Version currentVersion = Assembly.GetExecutingAssembly().GetName().Version;
+
+                // Get version from appsettings
+                var currentVersion = new Version(_configuration.GetValue<string>("app-version"));
+                var requestVersion = new Version(httpContext.Request.Headers["app-version"]);
+
+                if (requestVersion.CompareTo(currentVersion) <= 0 || httpContext.Request.Path == "/login" || httpContext.Request.Path == "/register")
+                {
+                    await _next(httpContext);
+                }
+                else
+                {
+                    httpContext.Response.StatusCode = 401;
+                    await httpContext.Response.WriteAsync("Unuthorized");
+                }
+            }
+            catch (Exception ex)
+            {
+                await HandleExcepitonAsync(httpContext, ex);
+            }
+        }
+        private async Task HandleExcepitonAsync(HttpContext context, Exception exception)
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync($"Unauthorized: {exception.Message}");
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class AppVersionControllerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseAppVersionControllerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AppVersionControllerMiddleware>();
+        }
+    }
+}
